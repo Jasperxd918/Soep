96a16dc7d6b7b386e482e696dcec2c5537fc2d84
Soepski <ronlieb@hotmail.com>
2016-11-23 10:09:02 +0100

    Ron's ferme update

--- Soep/Soep/Soep classes.cs
+++ Soep/Soep/Soep classes.cs
@@ -1,20 +1,44 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace Soep
 {
     class Soep_classes
     {
-        SqlConnection conn = new SqlConnection("Server = localhost; Database=SoepDB;Uid=root;Pwd=;");
-        
-        public class Login
-        {
+        MySqlConnection _mssCon = new MySqlConnection("Server = localhost; Database=SoepDB;Uid=root;Pwd=;");
 
+        public DataTable Inloggen()
+        {
+            DataTable dtResult = new DataTable();
+
+            try
+            {
+                _mssCon.Open();
+                if (_mssCon.State == ConnectionState.Open)
+                {
+                    MySqlCommand mssCommand = new MySqlCommand("SELECT * FROM jasper;", _mssCon);
+            dtResult.Load(mssCommand.ExecuteReader());
+                }
+    }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message, "Er ging iets fout");
+            }
+            finally
+            {
+                _mssCon.Close();
+            }
+
+
+            return dtResult;
         }
     }
 }
