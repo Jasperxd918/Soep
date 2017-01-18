48ddbed52c995217e7e92a41a9071a6ea2515b92
Soepski <ronlieb@hotmail.com>
2017-01-18 09:45:46 +0100

    heu

--- Soep/Functies.cs
+++ Soep/Functies.cs
@@ -3,11 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
 
 namespace Soep
 {
     class Functies
     {
+        public DataRow Inloggen(int iID)
+        {
+            DataTable dtResult = new DataTable();
 
+            try
+            {
+                _mssCon.Open();
+                if (_mssCon.State == ConnectionState.Open)
+                {
+                    MySqlCommand mssCommand = new MySqlCommand("SELECT * FROM jasper WHERE ID=@ID;", _mssCon);
+                    mssCommand.Parameters.AddWithValue("@ID", iID);
+                    dtResult.Load(mssCommand.ExecuteReader());
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message, "Er ging iets fout");
+            }
+            finally
+            {
+                _mssCon.Close();
+            }
+
+
+            return dtResult.Rows[0];
+        }
     }
 }
